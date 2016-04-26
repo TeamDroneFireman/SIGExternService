159d64753b20c08e1f884169e5f5b5f35ee77ab6
thomas_huchede <huchede.thomas@gmail.com>
2016-04-26 17:25:28 +0200

    Fix path

--- server/boot/health.js
+++ server/boot/health.js
@@ -1,6 +1,6 @@
 module.exports = function(server) {
   var router = server.loopback.Router();
-  router.get('/extern/api/SIGs/health', function(req, res) {
+  router.get('/api/SIGExterns/health', function(req, res) {
     res.status(200).send({status:'OK'});
   });
   server.use(router);
--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 var supertest = require('supertest');
 var expect = require('chai').expect;
 
-var server = supertest('http://localhost:3007/extern/api/SIGs');
+var server = supertest('http://localhost:3007/api/SIGExterns');
 
 describe('Server health response', function () {
   it('should return 200 OK', function (done) {
