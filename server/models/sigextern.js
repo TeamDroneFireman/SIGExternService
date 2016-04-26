78f4e09ac941abac26842e3ba49add606336c215
thomas_huchede <huchede.thomas@gmail.com>
2016-04-26 16:21:38 +0200

    Change port for local

--- server/models/sigextern.js
+++ server/models/sigextern.js
@@ -1,10 +1,10 @@
 module.exports = function(SigExtern) {
-  
+
   SigExtern.disableRemoteMethod('create',true);
   SigExtern.disableRemoteMethod('deleteById', true);
   SigExtern.disableRemoteMethod('updateAll', true);
   SigExtern.disableRemoteMethod('createChangeStream', true);
   SigExtern.disableRemoteMethod('findOne', true);
   SigExtern.disableRemoteMethod('exists', true);
-  
+
 };
