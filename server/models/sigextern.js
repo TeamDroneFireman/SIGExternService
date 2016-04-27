4881eed01cb550733aa32d73cae0453128712428
Gweltaz GESTIN <gestin.gweltaz@gmail.com>
2016-04-27 14:33:05 +0200

    correction bouchon

--- server/models/sigextern.js
+++ server/models/sigextern.js
@@ -9,19 +9,19 @@ module.exports = function(SigExtern) {
     switch (address) {
       case '263 Avenue Général Leclerc, 35700 Rennes, France' :
         means=[
-          {'type': 'string', 'location': '48.114121, -1.636343'},
-          {'type': 'string', 'location': '48.112228, -1.640710'},
-          {'type': 'string', 'location': '48.113218, -1.650110'},
-          {'type': 'string', 'location': '48.115228, -1.642710'}
+          {'type': 'string', 'location': {'lat' : 48.114121,'long' : -1.636343}},
+          {'type': 'string', 'location': {'lat' : 48.112228,'long' : -1.640710}},
+          {'type': 'string', 'location': {'lat' : 48.113218,'long' : -1.650110}},
+          {'type': 'string', 'location': {'lat' : 48.115228,'long' : -1.642710}}
         ];
         break;
       case 'Place du Parlement de Bretagne, 35000 Rennes, France' :
         means=[
-          {'type': 'string', 'location': '48.113743, -1.677971'},
-          {'type': 'string', 'location': '48.108743, -1.679821'},
-          {'type': 'string', 'location': '48.109743, -1.674821'},
-          {'type': 'string', 'location': '48.112743, -1.677621'},
-          {'type': 'string', 'location': '48.113443, -1.676021'}
+          {'type': 'string', 'location': {'lat' : 48.113743,'long' : -1.677971}},
+          {'type': 'string', 'location': {'lat' : 48.108743,'long' : -1.679821}},
+          {'type': 'string', 'location': {'lat' : 48.109743,'long' : -1.674821}},
+          {'type': 'string', 'location': {'lat' : 48.112743,'long' : -1.677621}},
+          {'type': 'string', 'location': {'lat' : 48.113443,'long' : -1.676021}}
         ];
         break;
     }
