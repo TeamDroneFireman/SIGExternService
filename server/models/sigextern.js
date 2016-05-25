0280f2c846d962489723d491044dd9f10b639a26
Gweltaz GESTIN <gestin.gweltaz@gmail.com>
2016-05-25 10:20:57 +0200

    Ajout form pour sigextern

--- server/models/sigextern.js
+++ server/models/sigextern.js
@@ -9,50 +9,30 @@ module.exports = function(SigExtern) {
     switch (address) {
       case '263 Avenue Général Leclerc, 35700 Rennes, France' :
         means=[
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'WATERPOINT_SUSTAINABLE','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.114121,'long' : -1.636343}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'WATERPOINT','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.112228,'long' : -1.640710}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'WATERPOINT_SUSTAINABLE','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.113218,'long' : -1.650110}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'SOURCE','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.115228,'long' : -1.642710}}
         ];
         break;
       case 'Place du Parlement de Bretagne, 35000 Rennes, France' :
         means=[
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'WATERPOINT_SUSTAINABLE','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.113743,'long' : -1.677971}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'SOURCE','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.108743,'long' : -1.679821}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'WATERPOINT_SUSTAINABLE','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.109743,'long' : -1.674821}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'WATERPOINT','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.112743,'long' : -1.677621}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
+          {'form': 'SOURCE','role':'string', 'isExternal':true, 'location':
             {'lat' : 48.113443,'long' : -1.676021}}
         ];
         break;
-      default: means=[
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.113743,'long' : -1.677971}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.108743,'long' : -1.679821}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.109743,'long' : -1.674821}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.112743,'long' : -1.677621}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.113443,'long' : -1.676021}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.114121,'long' : -1.636343}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.112228,'long' : -1.640710}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.113218,'long' : -1.650110}},
-          {'form': 'string','role':'string', 'isExternal':true, 'location':
-        {'lat' : 48.115228,'long' : -1.642710}}
-      ];
     }
     cb(null, means);
   };
